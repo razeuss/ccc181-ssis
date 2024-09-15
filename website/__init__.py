96964aadac0073d817ca2d42f4e0a8ff7757cc56
Joshua Radz Adlaon <joshuaradz.adlaon@g.msuiit.edu.ph>
2024-09-16 04:31:49 +0800

    Modified some files and also added the abstract html form of the homepage and add function

--- website/__init__.py
+++ website/__init__.py
@@ -2,4 +2,11 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
+    # Import the blueprint
+    from .routes import views
+
+    # Register the blueprint (without url_prefix)
+    app.register_blueprint(views)
+
     return app
