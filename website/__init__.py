43e461142000929f1c443ad00a9f51c42fb70230
Joshua Radz Adlaon <joshuaradz.adlaon@g.msuiit.edu.ph>
2024-09-22 01:12:33 +0800

    added the student table and mysql in init.py and routes.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+
 
 def create_app():
     app = Flask(__name__)
-
+    
+    app.secret_key = 'joshua'
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'joshua'
+    app.config['MYSQL_DB'] = 'ssis'
+    
+    mysql = MySQL(app)
+    
     from .routes import views
     app.register_blueprint(views)
 
--- website/routes.py
+++ website/routes.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
+
+views = Blueprint('views', __name__)
+mysql = MySQL()
+
 
 views = Blueprint('views', __name__)
 
@@ -6,9 +11,28 @@ views = Blueprint('views', __name__)
 def homepage():
     return render_template("homepage.html")
 
-@views.route('/add')
+@views.route('/add', methods=['GET', 'POST'])
 def addstud():
-    return render_template("addstud.html")
+    if request.method == 'POST':
+       
+        student_id = request.form['studentID']
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        program = request.form['program']
+        year = request.form['year']
+        gender = request.form['gender']
+
+     
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO student (id, firstname, lastname, program_code, year, gender) VALUES (%s, %s, %s, %s, %s, %s)", 
+                    (student_id, first_name, last_name, program, year, gender))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Student Added Successfully', 'success')
+        return redirect(url_for('views.students_list'))
+
+    return render_template("studentslist.html")
 
 @views.route('/programs')
 def update():
@@ -16,4 +40,12 @@ def update():
 
 @views.route('/Students')
 def students_list():
-    return render_template("studentslist.html")
+    cur = mysql.connection.cursor()
+    # Query to fetch all students
+    cur.execute("SELECT firstname, lastname, id, program_code, gender, year FROM student")
+    students = cur.fetchall()  # Fetch all rows from the query result
+    cur.close()
+
+    # Pass the 'students' list to the template
+    return render_template('studentslist.html', students=students)
+
