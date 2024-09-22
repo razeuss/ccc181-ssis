04203d2083d447b705339198d4838494493b06ac
Joshua Radz Adlaon <joshuaradz.adlaon@g.msuiit.edu.ph>
2024-09-22 23:15:51 +0800

    updated the templates and modified some codes

--- website/routes.py
+++ website/routes.py
@@ -9,7 +9,8 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def homepage():
-    return render_template("homepage.html")
+    return render_template("Master Layout/homepage.html")
+
 
 @views.route('/add', methods=['GET', 'POST'])
 def addstud():
@@ -32,20 +33,85 @@ def addstud():
         flash('Student Added Successfully', 'success')
         return redirect(url_for('views.students_list'))
 
-    return render_template("studentslist.html")
+    return render_template("Student Layout/studentslist.html")
 
-@views.route('/programs')
-def update():
-    return render_template("programs.html")
 
 @views.route('/Students')
 def students_list():
     cur = mysql.connection.cursor()
-    # Query to fetch all students
+    
     cur.execute("SELECT firstname, lastname, id, program_code, gender, year FROM student")
-    students = cur.fetchall()  # Fetch all rows from the query result
+    students = cur.fetchall()  
     cur.close()
 
-    # Pass the 'students' list to the template
-    return render_template('studentslist.html', students=students)
+   
+    return render_template('Student Layout/studentslist.html', students=students)
+
+
+
+@views.route('/programs')
+def update():
+    cur = mysql.connection.cursor()
+   
+    cur.execute("SELECT code, name, college_code FROM program")
+    programs = cur.fetchall() 
+    cur.close()
+
+    
+    return render_template('Program layout/programs.html', programs=programs)
+
+
+
+@views.route('/add_program', methods=['GET', 'POST'])
+def add_program():
+    if request.method == 'POST':
+       
+        coursecode = request.form['programcode']
+        coursename = request.form['programname']
+        collegecode = request.form['collegecode']
+        
+
+     
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO program (code, name, college_code) VALUES (%s, %s, %s)", 
+                    (coursecode, coursename, collegecode))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Program Added Successfully', 'success')
+        return redirect(url_for('views.update'))
+    
+    return render_template("Program layout/programs.html")
+
+
+@views.route('/add_college', methods=['GET', 'POST'])
+def add_college():
+    if request.method == 'POST':
+       
+        collegecode = request.form['programname']
+        collegename = request.form['collegecode']
+        
+
+     
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO college (code, name) VALUES (%s, %s)", 
+                    (collegecode, collegename))
+        mysql.connection.commit()
+        cur.close()
+
+        flash('Program Added Successfully', 'success')
+        return redirect(url_for('views.college'))
+    
+    return render_template("College Layout/college.html")
+    
+@views.route('/college')
+def college():
+    cur = mysql.connection.cursor()
+   
+    cur.execute("SELECT code, name FROM college")
+    colleges = cur.fetchall() 
+    cur.close()
+
+    
+    return render_template('College Layout/college.html', colleges=colleges)
 
