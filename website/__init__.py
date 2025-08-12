9ee565a9b184de388d672e828a6acc3e0111dc59
Joshua Radz Adlaon <joshuaradz.adlaon@g.msuiit.edu.ph>
2025-08-12 12:18:09 +0800

    finish

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,11 @@ from flask_mysqldb import MySQL
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-from config import SECRET_KEY, MYSQL_DB, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_USER, CLOUD_API_KEY, CLOUD_API_SECRET, CLOUD_NAME
+from config import ( SECRET_KEY, MYSQL_DB, MYSQL_HOST, MYSQL_PASSWORD, 
+                    MYSQL_USER, CLOUD_API_KEY, CLOUD_API_SECRET, CLOUD_NAME,
+                    MYSQL_PORT, MYSQL_UNIX_SOCKET, MYSQL_READ_DEFAULT_FILE,
+                    MYSQL_CONNECT_TIMEOUT, MYSQL_USE_UNICODE, MYSQL_CHARSET
+                    , MYSQL_SQL_MODE, MYSQL_CURSORCLASS, MYSQL_AUTOCOMMIT)
 
 cloudinary.config(
     cloud_name=CLOUD_NAME,
@@ -21,7 +25,17 @@ def create_app():
     app.config['MYSQL_PASSWORD'] = MYSQL_PASSWORD
     app.config['MYSQL_DB'] = MYSQL_DB
     
-    mysql = MySQL(app)
+    app.config['MYSQL_PORT'] = MYSQL_PORT
+    app.config['MYSQL_UNIX_SOCKET'] = MYSQL_UNIX_SOCKET
+    app.config['MYSQL_CONNECT_TIMEOUT'] = MYSQL_CONNECT_TIMEOUT
+    app.config['MYSQL_READ_DEFAULT_FILE'] = MYSQL_READ_DEFAULT_FILE
+    app.config['MYSQL_USE_UNICODE'] = MYSQL_USE_UNICODE
+    app.config['MYSQL_CHARSET'] = MYSQL_CHARSET
+    app.config['MYSQL_SQL_MODE'] = MYSQL_SQL_MODE
+    app.config['MYSQL_CURSORCLASS'] = MYSQL_CURSORCLASS
+    app.config['MYSQL_AUTOCOMMIT'] = MYSQL_AUTOCOMMIT
+    app.config['MYSQL_CUSTOM_OPTIONS'] = {}
+
     
     from .main_route import views
     app.register_blueprint(views)
